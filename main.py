f5c8747492b90405abfbab8d90e72dc60fa49c33
Never11n <never1n@ukr.net>
2023-07-04 01:18:33 +0300

    add an exeption

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer("Привіт! Я Нелінійний бот. Мене стоврили для вирішення нелінійних рівнянь та інших прикладів.")
+    await message.answer("Привіт! Я Нелінійний бот. Мене стоврили для вирішення нелінійних рівнянь та інших прикладів.\nВведіть ваш приклад")
 
 @dp.message_handler()
 async def echo_message(message: types.Message):
     equation = message.text
-    res = client.query(equation)
-    output = next(res.results).text
-    await message.answer(output)
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    try:
+        res = client.query(equation)
+        output = next(res.results).text
+        await message.answer(output)
+    except Exception as e:
+        await message.answer("Ви ввели невірні данні, спробуйте ще раз.")
+    
+    
+    
+    
+executor.start_polling(dp)
     
     
     
