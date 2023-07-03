7f9f4638062bdccd5fd127d1891ec1f3b444647c
Never11n <never1n@ukr.net>
2023-07-04 00:06:24 +0300

    Add WolframAlpha into ChatBot

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from aiogram import Bot, Dispatcher, executor, types
+import wolframalpha
+
+client = wolframalpha.Client("QPQT9K-UQH266YG92")
+
+
 
 bot = Bot('6356964227:AAGnMM33z6JWgAzajB7vnhfVfvgFZ7sHFZU')
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands='start')
-async def start(message: types.Message):
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
     await message.answer("Привіт! Я Нелінійний бот. Мене стоврили для вирішення нелінійних рівнянь та інших прикладів.")
 
 @dp.message_handler()
-async def strHandle(message):
-    str_equation = message
-    await message.answer(str_equation)
+async def echo_message(message: types.Message):
+    equation = message.text
+    res = client.query(equation)
+    output = next(res.results).text
+    await message.answer(output)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
     
-
-executor.start_polling(dp)
-
+    
+    
